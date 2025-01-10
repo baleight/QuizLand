fcc7ac7c4048d63f43c32aace956718c8b3f7ff4
baleight <baleight2@gmail.com>
2025-01-10 11:34:06 +0100

    add admin

--- quizland/quizapp/admin.py
+++ quizland/quizapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Question, QuizSession, QuizType
 
-# Register your models here.
+# Registrazione dei modelli al pannello di amministrazione di Django.
+# Questo permette agli amministratori di gestire i dati direttamente dal pannello admin.
+
+# Il modello Question rappresenta le domande associate ai quiz.
+admin.site.register(Question)
+
+# Il modello QuizSession tiene traccia delle risposte degli utenti durante un quiz.
+admin.site.register(QuizSession)
+
+# Il modello QuizType definisce i tipi di quiz disponibili nell'applicazione.
+admin.site.register(QuizType)
--- quizland/quizapp/apps.py
+++ quizland/quizapp/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
+# Configurazione dell'applicazione Django per la gestione dei quiz.
+# Questa configurazione definisce il comportamento dell'app all'interno del progetto.
 
-class QuizappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'quizapp'
+class QuizConfig(AppConfig):
+    # Definizione del tipo di chiave primaria predefinita per i modelli.
+    default_auto_field = "django.db.models.BigAutoField"
+
+    # Nome dell'applicazione, che deve corrispondere al nome della directory contenente questa configurazione.
+    name = "quiz"
