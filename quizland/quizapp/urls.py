49007f5606a0c6afa3fb264deb9339c525a49f05
baleight <baleight2@gmail.com>
2025-01-10 11:35:35 +0100

    add urls frontend

--- quizland/quizapp/urls.py
+++ quizland/quizapp/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path
 from . import views
 
+# Definizione dello spazio dei nomi per le URL dell'applicazione.
+# Questo consente di distinguere le rotte di questa app da quelle di altre app nel progetto.
+app_name = 'quiz'
+
+# Definizione delle rotte URL per l'app Quiz.
 urlpatterns = [
-    path('', views.hello_world, name='hello_world'),
+    path('', views.home, name='home'),  # Homepage dell'app.
+    path('start/<int:quiz_type_id>/', views.StartQuizView.as_view(), name='start_quiz'),  # Inizio di un quiz.
+    path('submit_answer/<int:question_id>/', views.SubmitAnswerView.as_view(), name='submit_answer'),  # Invio di una risposta.
+    path('results/<int:quiz_type_id>/', views.QuizResultView.as_view(), name='quiz_results'),  # Visualizzazione dei risultati.
+    path('reset/<int:pk>/', views.ResetQuizView.as_view(), name='reset_quiz'),  # Reset di un quiz.
+    path('manage-quiz/', views.manage_quiz, name="manage-quiz"),  # Gestione dei quiz per gli amministratori.
+    path('add-quiz/', views.add_quiz, name="add_quiz"),  # Aggiunta di un nuovo quiz.
+    path('update-quiz/<int:pk>/', views.update_quiz, name="update_quiz"),  # Modifica di un quiz esistente.
+    path('delete-quiz/<int:pk>/', views.delete_quiz, name="delete_quiz"),  # Eliminazione di un quiz.
+    path('add-question/<int:pk>/', views.add_questions, name='add_question'),  # Aggiunta di una domanda a un quiz.
+    path('update-question/<int:pk>/', views.update_question, name="update_question"),  # Modifica di una domanda.
+    path('delete-question/<int:pk>/', views.delete_question, name='delete_question'),  # Eliminazione di una domanda.
+    path('questions/manage/<int:pk>/', views.manage_questions, name='manage_questions'),  # Gestione delle domande.
 ]
